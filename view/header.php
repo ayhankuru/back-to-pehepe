0ade7e10a16f28d69ab764d2198b0de652dedcb1
ayhankuru <cobaimelan@yandex.com.tr>
2014-12-21 05:44:50 +0200

    fix

--- view/header.php
+++ view/header.php
@@ -1,9 +1,9 @@
 <html>
 	<head>
-		<link rel="stylesheet" href="{uri}/public/assets/bootstrap/dist/css/bootstrap.css">
+		<link rel="stylesheet" href="{uri}/public/assets/todc-bootstrap/dist/css/todc-bootstrap.css">
 		<link rel="stylesheet" href="{uri}/public/assets/style.css">
 		<script src="{uri}/public/assets/jquery/dist/jquery.min.js"></script>
-		<script src="{uri}/public/assets/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="{uri}/public/assets/todc-bootstrap/dist/js/bootstrap.min.js"></script>
 		<title> {title} </title>
 	</head>
 <body>
